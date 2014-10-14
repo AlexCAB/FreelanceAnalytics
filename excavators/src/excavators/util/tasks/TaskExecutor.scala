21d87dee3a3d1432f62506af73054777d83854ea
CAB <cabsoftbox@gmail.com>
2014-10-14 19:07:11 +0300

    Work v0.0.8

--- excavators/src/excavators/util/tasks/TaskExecutor.scala
+++ excavators/src/excavators/util/tasks/TaskExecutor.scala
@@ -1,5 +1,5 @@
 package excavators.util.tasks
-import scala.collection.mutable.Queue
+import scala.collection.mutable.{Set => MutSet}
 
 /**
  * Execute task form task queue
@@ -7,12 +7,13 @@ import scala.collection.mutable.Queue
  */
 
 trait TaskExecutor{
+  //Parameters
+  val checkTimeOut = 1000L
+  val endWorkTimeOut = 600000L
   //Variables
   private var work = false
   private var paused = false
-  private val taskQueue = Queue[Task]()
-  //Functions
-
+  private val taskQueue = MutSet[Task]()
   //Methods
   def start() = executorThread.synchronized{
     work = true
@@ -20,22 +21,37 @@ trait TaskExecutor{
   def stop() = {
     executorThread.synchronized{
       work = false
-      notify()}
-    executorThread.join(600000)}
+      executorThread.notify()}
+    executorThread.join(endWorkTimeOut)}
   def setPaused(f:Boolean) = {
-    if(f){executorThread.synchronized{notify()}}
+    if(! f){executorThread.synchronized{executorThread.notify()}}
     paused = f}
   def addTask(t:Task) = {
-
-  }
+    taskQueue.synchronized{taskQueue += t}
+    executorThread.synchronized{executorThread.notify()}}
   //Thread
   private val executorThread = new Thread{override def run() = {
-
-
-
-
-
-
-
-  }}
+    while(work){
+      if(! paused){
+        //Search next ready task and run
+        val (nt,tm) = taskQueue.synchronized{
+          if(taskQueue.nonEmpty){
+            val ct = System.currentTimeMillis()
+            val mt = taskQueue.map(_.time - ct).min
+            if(mt <= 0){
+              (Some(taskQueue.filter(_.time <= ct).maxBy(_.priority)), 0L)} //Get next task
+            else{
+              (None, mt)}} //No task at current time
+          else{
+            (None, checkTimeOut)}} //No task in queue then waite
+        //Execute task or wait
+        nt match{
+          case Some(t) => {
+            taskQueue -= t
+            t.execute()}             //Execute task
+          case None => {
+            synchronized{wait(tm)}}}} //If no ready task then wait
+      else{
+        //If paused then white
+        synchronized{wait(checkTimeOut)}}}}}
   executorThread.setDaemon(true)}
--- excavators/test/excavators/util/tasks/TaskExecutorTest.scala
+++ excavators/test/excavators/util/tasks/TaskExecutorTest.scala
@@ -19,30 +19,46 @@ class TaskExecutorTest extends WordSpecLike with Matchers {
     Thread.sleep(100)
     assert(fl == true)}
   "should execute task with time out" in {
-
-
-  }
+    var fl = false
+    val t1 = new Task(System.currentTimeMillis() + 2000, 1){def execute() = {fl = true}}
+    executor.addTask(t1)
+    Thread.sleep(1000)
+    assert(fl == false)
+    Thread.sleep(1100)
+    assert(fl == true)}
   "should execute task by priority" in {
-
-
-  }
-  "should paused" in{
-
-  }
-  "should run after un pause" in{
-
-
-
-  }
+    var fl = false
+    val t = System.currentTimeMillis() + 100
+    val t1 = new Task(t, 1){def execute() = {fl = true}}
+    val t2 = new Task(t, 2){def execute() = {Thread.sleep(1000)}}
+    executor.addTask(t1)
+    executor.addTask(t2)
+    Thread.sleep(500)
+    assert(fl == false)
+    Thread.sleep(1100)
+    assert(fl == true)}
+  "should paused and should run after un pause" in{
+    var fl = false
+    val t = System.currentTimeMillis()
+    val t1 = new Task(t + 100, 2){def execute() = {Thread.sleep(500)}}
+    val t2 = new Task(t + 200, 1){def execute() = {fl = true}}
+    executor.addTask(t1)
+    executor.addTask(t2)
+    Thread.sleep(400)
+    executor.setPaused(true)
+    Thread.sleep(400)
+    assert(fl == false)
+    executor.setPaused(false)
+    Thread.sleep(10)
+    assert(fl == true)}
   "should stop only after end current task" in {
-
-
-
-
-  }
-
-
-
-
-
-}
+    var fl = false
+    val ct = System.currentTimeMillis()
+    val t1 = new Task(ct, 1){def execute() = {
+      Thread.sleep(1000)
+      fl = true}}
+    executor.addTask(t1)
+    Thread.sleep(100)
+    executor.stop()
+    assert(fl == true)
+    assert(System.currentTimeMillis() > ct + 900)}}
